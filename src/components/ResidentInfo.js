b8b01348e689e965872b89971547603f8d40b53f
LuisCarrilloP <jl.carrillopadron@gmail.com>
2022-05-16 17:06:32 -0500

    status color

--- src/components/ResidentInfo.js
+++ src/components/ResidentInfo.js
@@ -19,7 +19,7 @@ const ResidenInfo = ({resident}) => {
             <ul>
                 <li key={character.image}><img src={character.image} alt="" /* style={{width:("250px")}} *//></li>
                 <li key={character.name} className="character-name"> {character.name}</li>
-                <li key={character.status} className="character-info"><b>Status:</b> {character.status}</li>
+                <li key={character.status} className="character-info"><b>Status:</b> <span style={{color:  character.status === "Dead" ? "red" : character.status === "Alive" ? "lightgreen" : "gray"}}>{character.status}</span>{/* <div className='character-status' style={{background: character.status === "Dead" ? "red" : character.status === "Alive" ? "green" : "gray"}}></div> */}</li>
                 <li key={character.origing?.name} className="character-info"><b>Origin:</b> {character.origin?.name}</li>
                 <li key={character.episode?.length} className="character-info"><b>Episodios:</b> {character.episode?.length}</li>
             </ul>
